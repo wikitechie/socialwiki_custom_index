03f3201a5e7cde63dc736c265598c95c51691237
alAmjadTawfiq Isstaif <isstaif@gmail.com>
2011-09-12 22:51:54 +0300

    first commit

---
+++ views/default/custom_index/css.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Index CSS
+ *
+ */
+?>
+
+/*******************************
+	Custom Index
+********************************/
+.custom-index {
+	padding: 10px 0;
+}
+.custom-index .elgg-module-featured {
+	-webkit-box-shadow: 1px 1px 5px #CCC;
+	-moz-box-shadow: 1px 1px 5px #CCC;
+	box-shadow: 1px 1px 5px #CCC;
+}
+.custom-index .elgg-module-featured:hover {
+	-webkit-box-shadow: 1px 1px 6px #AAA;
+	-moz-box-shadow: 1px 1px 6px #AAA;
+	box-shadow: 1px 1px 6px #AAA;
+}
